1df09e807a145b32be847098a33c47d71a4e9dca
Jory <46983170+Cambro320@users.noreply.github.com>
2021-03-09 13:32:20 -0600

    fixed subnav accordion effect

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const menuItems = document.getElementById('menu-items');
 const mainNav = document.getElementById('main-nav');
 const sticky = mainNav.offsetTop;
 
+// if menu-items max-height, menu-items.style.maxHeight = null
+// else menu-items.style.maxHeight = panel.scrollHeight + "px"
+// ergo, if menu-items is showing, a click changes MH to null, hiding it
+// if menu-items is hidden, a click changes MH to menu-items' scrollHeight in px
 menuIcon.addEventListener('click', () => {
-  menuItems.classList.toggle('inactive');
+  if (menuItems.style.maxHeight) {
+    menuItems.style.maxHeight = null;
+  } else {
+    menuItems.style.maxHeight = menuItems.scrollHeight + "px";
+  }
 });
 
 window.onscroll = function () {
@@ -18,3 +26,4 @@ function stickyNav() {
     mainNav.classList.remove("sticky");
   }
 }
+
